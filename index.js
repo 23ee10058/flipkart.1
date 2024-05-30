f953154f609ef4a3db1690b42ad942b984f8ccca
23ee10058 <146566370+23ee10058@users.noreply.github.com>
2024-05-30 19:04:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function todo(){
+    const select=document.createElement('select');
+
+    const option=document.createElement('option');
+    option.innerHTML='My Profile'
+    select.appendChild(option)
+
+}
